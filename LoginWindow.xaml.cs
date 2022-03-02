1b1cd01092c345c3bc810d0b126a704d7d073266
Lukich <55893122+Kerbx@users.noreply.github.com>
2022-03-02 10:34:06 +0300

    Created Login window, two buttons

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kurwa1
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            this.Closing += closeAll;
+        }
+
+        private void closeAll(object sender, EventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Close();
+        }
+        private void CloseB_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinusB_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void singUpButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void singInButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,12 +26,17 @@ namespace kurwa1
         public MainWindow()
         {
             InitializeComponent();
-
+            this.Loaded += openLoginForm;
             // DataTable dtUser = ConnectToDatabase("SELECT * FROM [dbo].[users];");
             
         }
 
-
+        private void openLoginForm(object sender, EventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
+        
         private void CloseB_Click(object sender, MouseButtonEventArgs e)
         {
             this.Close();
@@ -41,75 +46,71 @@ namespace kurwa1
         {
             this.WindowState = WindowState.Minimized;
         }
+        private DataTable ConnectToDatabase(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("dataBase");
+
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection("server=СЕМЬЯ-ПК;Trusted_Connection=Yes;DataBase=usersDatabase;");
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = selectSQL;
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(dataTable);
+
+                return dataTable;
+
+            } catch(Exception exception)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных.");
+                return dataTable;
+            };
+
+        }
+
+        private bool IsUserExists(DataTable dataTable, string username, string password)
+        {
+            for(int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                if(username == (string)dataTable.Rows[i][0])
+                {
+                    if(password == (string)dataTable.Rows[i][0])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неправильный пароль.");
+                        return false;
+                    }
+                }
+                else if (i < dataTable.Rows.Count){
+                    continue;
+                }
+                else
+                {
+                    MessageBox.Show("Такого пользователя не существует.");
+                    return false;
+                }
+            }
+
+            return false;
+        }
         /*
-private DataTable ConnectToDatabase(string selectSQL)
-{
-DataTable dataTable = new DataTable("dataBase");
+        private void Check(object sender, EventArgs e)
+        {
+            string username = textboxUsername.Text;
+            string password = textboxPassword.Text;
+            DataTable dtUser = ConnectToDatabase("SELECT * FROM [dbo].[users];");
 
-try
-{
-
-
-SqlConnection sqlConnection = new SqlConnection("server=СЕМЬЯ-ПК;Trusted_Connection=Yes;DataBase=usersDatabase;");
-sqlConnection.Open();
-
-SqlCommand sqlCommand = sqlConnection.CreateCommand();
-sqlCommand.CommandText = selectSQL;
-
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
-sqlDataAdapter.Fill(dataTable);
-
-return dataTable;
-
-} catch(Exception exception)
-{
-MessageBox.Show("Ошибка подключения к базе данных.");
-return dataTable;
-};
-
-}
-
-private bool IsUserExists(DataTable dataTable, string username, string password)
-{
-for(int i = 0; i < dataTable.Rows.Count; i++)
-{
-if(username == (string)dataTable.Rows[i][0])
-{
-if(password == (string)dataTable.Rows[i][0])
-{
-return true;
-}
-else
-{
-MessageBox.Show("Неправильный пароль.");
-return false;
-}
-}
-else if (i < dataTable.Rows.Count){
-continue;
-}
-else
-{
-MessageBox.Show("Такого пользователя не существует.");
-return false;
-}
-}
-
-return false;
-}
-
-private void Check(object sender, EventArgs e)
-{
-string username = textboxUsername.Text;
-string password = textboxPassword.Text;
-DataTable dtUser = ConnectToDatabase("SELECT * FROM [dbo].[users];");
-
-if(IsUserExists(dtUser, username, password))
-{
-MessageBox.Show("URAAAA");
-}
-}
-*/
+            if(IsUserExists(dtUser, username, password))
+            {
+                MessageBox.Show("URAAAA");
+            }
+        }*/
     }
 
 }
